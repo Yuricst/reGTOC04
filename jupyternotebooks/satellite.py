014591a389c757eed5bf59685bbb47c5e8f21ad4
Dev <dra16@ic.ac.uk>
2019-11-11 02:29:27 +0000

    updated satellite class to inherit functions from asteroid - something is wrong with meanAnom and eccAnom

--- jupyternotebooks/satellite.py
+++ jupyternotebooks/satellite.py
@@ -5,19 +5,44 @@ import scipy.optimize as opt
 from numpy import linalg as linalg
 from numpy.linalg import norm
 
-class Satellite:
+from asteroid import Asteroid
+
+class Satellite(Asteroid):
 
     def __init__(self):
 
         self.mu = 1.32712440018e+11  # gravitational parameter of Sun km3/s2
-        self.u_AU = 1.495978707e+8   # 1 AU in km
+        self.u_AU = 1.4959780691e+8   # 1 AU in km
         self.u_day = 86400           # 1 day in seconds
         self.u_deg2rad = np.pi/180   # conversion from degree to radians
 
         self.r = None
         self.v = None
 
-        self.name = 'Scaramouche'
+        self.name = 'Spacecraft Scaramouche'
+        self.epoch = None           # must be in MJD
+        self.a = None
+        self.e_mag = None
+        self.i = None
+        self.LAN = None
+        self.omega = None
+        self.M0 = None
+
+        #self.elements = (self.epoch, self.a, self.e_mag, self.i, self.LAN, self.omega, self.M0)
+
+        # cache the last calculated results
+        self.last_epoch_meanAnom = None
+        self.last_epoch_eccAnom = None
+        self.last_epoch_trueAnom = None
+        self.last_epoch_r_mag = None
+        self.last_epoch_gamma = None
+        self.last_epoch_v_mag = None
+        self.last_epoch_r = None
+        self.last_epoch_v = None
+
+    def get_state(self):
+
+        return self.epoch, self.r, self.v
 
     def set_state(self, epoch, r, v, add_to_hist=False):
 
@@ -25,39 +50,42 @@ class Satellite:
         self.r  = r
         self.v  = v
 
-        return None
+        # also update all orb elements
+        self.set_orb_elements(self.epoch, self.r, self.v)
 
-    def get_state(self):
-
-        return self.epoch, self.r, self.v
-
-    def state_to_orb_elements(self, epoch, r, v):
+    def set_orb_elements(self, epoch, r, v):
 
         #calculate angular momentum
-        h = np.cross(r,v)
-        print(h)
+        self.h = h = np.cross(r,v)
 
         #calculate inclination
-        i = np.arccos(h[2]/norm(h))
+        self.i = i = np.arccos(h[2]/norm(h))
 
         # eccentricity
-        e = np.cross(v, h)/self.mu - r/norm(r)
-        e_mag = norm(e)
+        self.e = e = np.cross(v, h)/self.mu - r/norm(r)
+        self.e_mag = e_mag = norm(e)
+
+        # semi major axis
+        self.a = a = (norm(h)**2/self.mu)/(1-e_mag**2)
 
         # right ascension
         K = np.array([0,0,1])
         N = np.cross(K,h)
-        print(N)
+
         if N[1] > 0: # if Ny > 0 then RA is the cosine
-            LAN = np.arccos(N[0]/norm(N))
+            self.LAN = LAN = np.arccos(N[0]/norm(N))
         else:        # if Ny < 0 then RA is 2*pi - cosine
-            LAN = 2*np.pi - np.arccos(N[0]/norm(N))
+            self.LAN = LAN = 2*np.pi - np.arccos(N[0]/norm(N))
+
+        self.LAN = self.LAN % (2*np.pi)
 
         # argument of perigee
         if e[2] > 0: # if ez > 0 then omega < 180
-            omega = np.arccos( np.dot(e,N)/(e_mag*norm(N)) )
+            self.omega = omega = np.arccos( np.dot(e,N)/(e_mag*norm(N)) )
         else:        # if ez < 0 then omega > 180
-            omega = 2*np.pi - np.arccos(np.dot(e,N)/(e_mag*norm(N)))
+            self.omega = omega = 2*np.pi - np.arccos(np.dot(e,N)/(e_mag*norm(N)))
+
+        self.omega = self.omega % (2*np.pi)
 
         # true anomaly
         v_radial = np.dot(v,r)/norm(r)  # radial velocity
@@ -66,17 +94,13 @@ class Satellite:
         else:
             theta = 2*np.pi - np.arccos(np.dot(e,r)/(e_mag*norm(r)))
 
+        theta =  theta % (2*np.pi)
+
+        self.theta = theta
 
         # calculate ecc anomaly
 
-        eccAnom = 2*np.arctan(np.sqrt((1-e_mag)/(1+e_mag))*np.tan(theta/2))
+        self.eccAnom = eccAnom = (2*np.arctan(np.sqrt((1-e_mag)/(1+e_mag))*np.tan(theta/2))) % (2*np.pi)
 
-        meanAnom = eccAnom - e_mag*np.sin(eccAnom)
-
-
-        elements = {'epoch':epoch,'h': h, 'i': i, 'e_vec': e, 'e': norm(e), 'LAN': LAN, 'omega': omega, 'theta': theta,'eccAnom':eccAnom, 'meanAnom': meanAnom}
-
-        return elements
-
-
-    
+        # calculate mean anom
+        self.M0 = (eccAnom - e_mag*np.sin(eccAnom)) % (2*np.pi)
