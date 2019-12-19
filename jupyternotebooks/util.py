e8ad91755ba25308722e2d83e50178b5bef5c2b1
Dev <dra16@ic.ac.uk>
2019-12-19 19:36:40 +0800

    exported node class, refined the n->n+1, defined node.local_opt

--- jupyternotebooks/util.py
+++ jupyternotebooks/util.py
@@ -4,6 +4,7 @@ import pandas as pd
 
 from orbit import Orbit, AU2km, mu, day2s
 
+
 def load_dataset(file = '../gtoc4_problem_data.txt'):
 
     df = pd.read_csv(file, skiprows=2,delimiter= '\s+',header=None)
@@ -28,3 +29,18 @@ def create_earth():
     earth.from_gtoc('Earth',54000,0.999988049532578, 1.671681163160e-2, 0.8854353079654e-3, 175.40647696473, 287.61577546182, 257.60683707535)
 
     return earth
+
+
+
+def select_asteroid(index):
+    """Helper function to get the asteroid as orbit object at given index"""
+    #maybe just using lists is easier?
+    #but this function can also be memoizeddddd
+    if index == -1:
+        return earth
+    return [ast for ast in asteroids if ast.index==index][0]
+
+
+
+asteroids, earth = load_dataset()
+asteroid_ind = set([ast.index for ast in asteroids])
